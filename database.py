f16dca36a56dcc4cd5b661cb8d91cd47f0007bb7
Shubham Sharma <shubham.1997.33@gmail.com>
2018-02-25 01:41:51 +0530

    data store functionality
    Form submission leads to storing the data into the database.

--- database.py
+++ database.py
@@ -18,22 +18,47 @@ class Students(Base):
 class staff(Base):
     __tablename__ = 'staff'
     id  = Column(Integer, primary_key = True)
+    Caretaker = Column(String(20), nullable = False)
+    Wardens = Column(String(20), nullable = False)
+    Principal= Column(String(20), nullable = False)
+    Behaviour = Column(String(20), nullable = False)
+    Problems = Column(String(20), nullable = False)
+    Rating = Column(String(20), nullable = False)
 
 class Water(Base):
     __tablename__ = 'room'
     id = Column(Integer, primary_key = True)
+    Water = Column(String(20), nullable = False)
+    Quality= Column(String(20), nullable = False)
+    Quantity = Column(String(20), nullable = False)
+    Variety = Column(String(20), nullable = False)
+    Cost = Column(String(20), nullable = False)
 
 class Mess(Base):
     __tablename__ = 'mess'
     id = Column(Integer, primary_key = True)
+    Morning = Column(String(20), nullable = False)
+    Afternoon = Column(String(20), nullable = False)
+    Evening = Column(String(20), nullable = False)
+    Dinner = Column(String(20), nullable = False)
+    Availablity = Column(String(20), nullable = False)
+    Quality = Column(String(20), nullable = False)
+    Behaviour = Column(String(20), nullable = False)
 
-class Facilities(Base):
+class FacilitiesFeedback(Base):
     __tablename__ = 'facilities'
     id = Column(Integer, primary_key = True)
+    Room = Column(String(20), nullable = False)
+    Corridors = Column(String(20), nullable = False)
+    Toilets = Column(String(20), nullable = False)
+    Croom = Column(String(20), nullable = False)
+    Lawns = Column(String(20), nullable = False)
 
-class Others(Base):
+class OtherFeedback(Base):
     __tablename__ = 'others'
     id = Column(Integer, primary_key = True)
+    Availability = Column(String(20), nullable = False)
+    Services = Column(String(20), nullable = False)
 
 engine = create_engine('sqlite:///feedback.db')
 Base.metadata.create_all(engine)
--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import Base
+from database import Base, OtherFeedback, FacilitiesFeedback, Water, Mess, staff
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///feedback.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
@@ -13,28 +13,78 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/feedback')
-def hello():
+def Feedback():
     return render_template('feedback.html')
 
-@app.route('/feedback/staff')
+@app.route('/feedback/staff', methods = ['GET', 'POST'])
 def StaffFeedback():
-    return render_template('staff.html')
+    if request.method == 'POST':
+        newFeedback = staff(Caretaker = request.form['Caretaker'],
+                            Wardens = request.form['Wardens'],
+                            Principal = request.form['Principal'],
+                            Behaviour = request.form['Behaviour'],
+                            Problems = request.form['Problems'],
+                            Rating = request.form['Rating'])
+        session.add(newFeedback)
+        session.commit()
+        return render_template('staff.html')
+    else:
+        return render_template('staff.html')
 
-@app.route('/feedback/room')
+@app.route('/feedback/water', methods=['GET', 'POST'])
 def WaterFeedback():
-    return render_template('water.html')
+    if request.method == 'POST':
+        newFeedback = Water(Water = request.form['Water'],
+                            Quality = request.form['Quality'],
+                            Quantity = request.form['Quantity'],
+                            Variety = request.form['Variety'],
+                            Cost = request.form['Cost'])
+        session.add(newFeedback)
+        session.commit()
+        return render_template('water.html')
+    else:
+        return render_template('water.html')
 
-@app.route('/feedback/mess')
+@app.route('/feedback/mess', methods=['GET', 'POST'])
 def MessFeedback():
-    return render_template('mess.html')
+    if request.method == 'POST':
+        newFeedback = Mess(Morning = request.form['Morning'],
+                           Afternoon = request.form['Afternoon'],
+                           Evening = request.form['Evening'],
+                           Dinner = request.form['Dinner'],
+                           Availablity = request.form['Availability'],
+                           Quality = request.form['Quality'],
+                           Behaviour = request.form['Behaviour'])
+        session.add(newFeedback)
+        session.commit()
+        return render_template('mess.html')
+    else:
+        return render_template('mess.html')
 
-@app.route('/feedback/facilities')
+@app.route('/feedback/facilities', methods = ['GET','POST'])
 def Facilities():
-    return render_template('facilities.html')
+    if request.method == 'POST':
+        newFeedback = FacilitiesFeedback(Room = request.form['Room'],
+                                        Corridors = request.form['Corridors'],
+                                        Toilets = request.form['Toilets'],
+                                        Croom = request.form['Croom'],
+                                        Lawns = request.form['Lawns'])
+        session.add(newFeedback)
+        session.commit()
+        return render_template('facilities.html')
+    else:
+        return render_template('facilities.html')
 
-@app.route('/feedback/others')
+@app.route('/feedback/others', methods = ['GET', 'POST'])
 def Others():
-    return render_template('others.html')
+    if request.method == 'POST':
+        newFeedback = OtherFeedback(Availability = request.form['Availability'],
+                                    Services = request.form['Services'])
+        session.add(newFeedback)
+        session.commit()
+        return render_template('others.html')
+    else:
+        return render_template('others.html')
 
 
 if __name__ == '__main__':
